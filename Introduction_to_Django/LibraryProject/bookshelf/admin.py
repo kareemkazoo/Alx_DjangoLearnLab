92793396001d236454d280e23d95ead25f471e1c
KareemKazoo <kimo19950425@gmail.com>
2025-08-18 02:43:35 +0300

    Added LibraryProject

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # الأعمدة اللي هتظهر
+    list_filter = ('author', 'publication_year')           # فلترة حسب المؤلف والسنة
+    search_fields = ('title', 'author')                    # البحث حسب العنوان أو المؤلف
+
+# تسجيل الـ model مع التخصيص
+admin.site.register(Book, BookAdmin)
---
+++ Introduction_to_Django/LibraryProject/bookshelf/admin_setup.md
@@ -0,0 +1,13 @@
+# Admin Setup for Book Model
+
+```python
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
+```
