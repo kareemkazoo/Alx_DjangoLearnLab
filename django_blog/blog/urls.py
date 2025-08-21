2da6cdddc3323a18eb8f9aa35efc9691cbd7d6d0
KareemKazoo <kimo19950425@gmail.com>
2025-08-22 02:44:59 +0300

    Fixed

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 # blog/urls.py
 from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     # --- Post URLs ---
@@ -15,6 +15,9 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+
+    # --- Tag URLs ---
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts_by_tag'),
 
     # --- Auth URLs ---
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
