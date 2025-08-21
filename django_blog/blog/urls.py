af5de24d1927067311f2d280fdc5b8430786435b
KareemKazoo <kimo19950425@gmail.com>
2025-08-22 02:41:45 +0300

    Fixed

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,23 +1,7 @@
-from django.urls import path
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-    PostByTagListView,
-)
-
-urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
-    path("tags/<slug:tag_slug>/", PostByTagListView.as_view(), name="posts_by_tag"),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-]
+# blog/urls.py
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # --- Post URLs ---
@@ -31,4 +15,9 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+
+    # --- Auth URLs ---
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
